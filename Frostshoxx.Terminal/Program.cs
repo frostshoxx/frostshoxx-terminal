a2a43ddf9fa5bdd225173e3258a97369998b416b
Patt Rojanasthien <patt.rojanas@gmail.com>
2024-02-12 08:56:05 -0800

    adjust project structure

--- Frostshoxx.Terminal/Program.cs
+++ Frostshoxx.Terminal/Program.cs
@@ -42,11 +42,17 @@ var fruit = AnsiConsole.Prompt(
         .Title("What's your [green]favorite fruit[/]?")
         .PageSize(10)
         .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-        .AddChoices(new[] {
-            "Apple", "Apricot", "Avocado", 
-            "Banana", "Blackcurrant", "Blueberry",
-            "Cherry", "Cloudberry", "Cocunut",
-        }));
+        .AddChoices([
+            "Apple",
+            "Apricot",
+            "Avocado",
+            "Banana",
+            "Blackcurrant",
+            "Blueberry",
+            "Cherry",
+            "Cloudberry",
+            "Cocunut",
+        ]));
 
 // Echo the fruit back to the terminal
 AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
