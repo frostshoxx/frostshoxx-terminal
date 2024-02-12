d9ff6889305d608a9f77cfc9bd8f28eb5c540c51
Patt Rojanasthien <patt.rojanas@gmail.com>
2024-02-11 22:37:20 -0800

    some more options

--- frostshoxx-terminal/Program.cs
+++ frostshoxx-terminal/Program.cs
@@ -35,4 +35,18 @@ layout["Bottom"].Update(
 
 // Render the layout
 AnsiConsole.Write(layout);
-Console.ReadLine();
+
+// Ask for the user's favorite fruit
+var fruit = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What's your [green]favorite fruit[/]?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices(new[] {
+            "Apple", "Apricot", "Avocado", 
+            "Banana", "Blackcurrant", "Blueberry",
+            "Cherry", "Cloudberry", "Cocunut",
+        }));
+
+// Echo the fruit back to the terminal
+AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
